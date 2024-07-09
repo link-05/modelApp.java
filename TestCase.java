f11a9dcfd19f6f38d848a508f924873764b06858
Link-05 <>
2024-07-09 02:28:47 -0400

    Added try catch and tried to do more coding for writing files.

--- TestCase.java
+++ TestCase.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
-import java.io.file.Files;
-import java.io.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestCase {
     public static void main(String[] args) {
         File file = new File(
-            "C:\\Users\\linke\\OneDrive\\Documents\\GitHub\\modelApp.java\\member.txt");
+            "C:\\Users\\linke\\OneDrive\\Documents\\GitHub\\modelApp.java\\TestCase.java");
+        try {
+            readFile(file)};
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Read file
-    public static void readFile(File file) throws Exception
+    public static void readFile(File file) 
     {
         int i;
-        // Remain true until there is nothing to read.
-        while((i - fr.read()) != -1)
+        Path fileName = Paths.get("C:\\Users\\linke\\OneDrive\\Documents\\GitHub\\modelApp.java\\TestCase.java");
+        //Remain true until there is nothing to read.
+        while((i - file.read()) != -1)
 
             // Print all the content of a file
             System.out.print((char)i);
@@ -27,6 +34,12 @@ public class TestCase {
         //Content of the file is assigned with this
         String text 
             = "Test text 1";
+        // Defining the file name for the file. Ai Generated
+        Path fileName = Paths.get("C:\\Users\\linke\\OneDrive\\Documents\\GitHub\\modelApp.java\\TestCase.txt");
+
+        // Write the text to the file
+        Files.write(fileName, text.getBytes());
+    
 
         // Defining the file name fo the file
             // Path fileName = Path.of(
