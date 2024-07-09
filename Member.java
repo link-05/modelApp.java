20a8872afddf77aecf294075b0a89301a2b45041
Link-05 <>
2024-07-09 02:17:45 -0400

    Written basic outline of member class and tried to practice reading and writing into files

--- Member.java
+++ Member.java
@@ -1,3 +1,51 @@
 public class Member {
-    private String name;
+    //A member will have a username, password, and id attached to them
+    private String username;
+    private String password;
+    private static double members = 0.0;
+    private String dateOfBirth;
+    private String id;
+    
+    public Member(String user, String pass, int day, int month, int year) {
+        username = user;
+        password = pass;
+        members++;
+        id = "" + members;
+        dateOfBirth = "" + month + day + year;
+    }
+
+    //Method to check if Username match. Used for comparing to input
+    public Boolean compareUsername(String thatUser) {
+        return this.username.equals(thatUser);
+    }
+
+    //Method to check if Password match. Used for comparing to input
+    public Boolean comparePassword(String thatPass) {
+        return this.password.equals(thatPass);
+    }
+
+    public String getUsername (){
+        return username;
+    }
+
+    public String getPassword (){
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setUsername(String newUsername) {
+        username = newUsername;
+    }
+
+    public void setPassword(String newPassword) {
+        password = newPassword;
+    }
+
 }
--- TestCase.java
+++ TestCase.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.file.Files;
+import java.io.file.Path;
+
 public class TestCase {
     public static void main(String[] args) {
+        File file = new File(
+            "C:\\Users\\linke\\OneDrive\\Documents\\GitHub\\modelApp.java\\member.txt");
+    }
 
+    //Read file
+    public static void readFile(File file) throws Exception
+    {
+        int i;
+        // Remain true until there is nothing to read.
+        while((i - fr.read()) != -1)
+
+            // Print all the content of a file
+            System.out.print((char)i);
+    }
+
+    //Read and write file
+    public static void writeFile(File file) throws IOException
+    {
+        //Content of the file is assigned with this
+        String text 
+            = "Test text 1";
+
+        // Defining the file name fo the file
+            // Path fileName = Path.of(
+                
+            // )
     }
 }
